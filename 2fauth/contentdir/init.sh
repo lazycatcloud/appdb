0497aaca6213953f2936dcaa6f4f2200e8b14ef0
runui <pbk6@pbk6.cn>
2025-06-03 10:55:53 +0800

    fix(2fauth): data storage directory that handles errors

--- 2fauth/contentdir/init.sh
+++ 2fauth/contentdir/init.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 ###
- # @Author: Bin
- # @Date: 2024-10-26
- # @FilePath: /appdb/2fauth/contentdir/init.sh
-### 
+# @Author: Bin
+# @Date: 2024-10-26
+# @FilePath: /appdb/2fauth/contentdir/init.sh
+###
 set -eu
 
-mkdir -p /lzcapp/run/mnt/home/2fauth
+OLD_DATA_DIR=/lzcapp/run/mnt/home/2fauth
+NEW_DATA_DIR=/lzcapp/var/data
 
-# link: https://docs.2fauth.app/getting-started/installation/docker/docker-cli/#docker-cli-setup
-chown -R 1000:1000 /lzcapp/run/mnt/home/2fauth
-chmod -R 700 /lzcapp/run/mnt/home/2fauth
+mkdir -p "$NEW_DATA_DIR"
+if [ -d "$OLD_DATA_DIR" ]; then
+    mv "$OLD_DATA_DIR/"* "$NEW_DATA_DIR/"
+    echo "[Init] migrate data"
+    rm -rf "$OLD_DATA_DIR"
+else
+    echo "[Init] create data"
+fi
+
+if [ $(stat -c '%u' "/2fauth") != "1000" ]; then
+    # link: https://docs.2fauth.app/getting-started/installation/docker/docker-cli/#docker-cli-setup
+    chown -R 1000:1000 "$NEW_DATA_DIR"
+    chmod -R 700 "$NEW_DATA_DIR"
+    echo "[Init] chown data"
+fi
