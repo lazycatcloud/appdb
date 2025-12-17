9ec58f80fde240e611fd8ad1c0105e084db005bf
runui <pbk6@pbk6.cn>
2025-12-17 14:26:36 +0800

    chore(uptime-kuma): bump uptime-kuma:2.0.2

--- uptime-kuma/build.sh
+++ uptime-kuma/build.sh
@@ -3,16 +3,19 @@
 set -ex
 
 name='uptime-kuma'
-commit_hash='0f22dc251699a4f773f6d7cc7ddfb8d5c5d44f01'
+commit_hash='e0e2195f5c01591f289e2ae1b19c5cd0b75e909a'
 clone_url="git@gitee.com:lazycatcloud/${name}.git"
 image="registry.lazycat.cloud/appdb/${name}"
-image_version='v1.23.2-lzc1'
+image_version='v2.0.2-lzc3'
 image_tag="${image}:${image_version}"
 
-git clone ${clone_url}
+if [ ! -d "$name"]; then
+    git clone ${clone_url}
+fi
 pushd ${name}
+git fetch --all
 git checkout ${commit_hash}
-
-docker build -f Dockerfile -t "${image_tag}" ./uptime-kuma
-docker push "${image_tag}"
 popd
+
+docker build --platform linux/amd64 -f Dockerfile -t "${image_tag}" ./uptime-kuma
+docker push "${image_tag}"
